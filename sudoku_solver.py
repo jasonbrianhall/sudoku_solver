f5848c9a1a8ec08c58546024b94c686d51bece5f
jasonbrianhall@gmail.com <jbhall@fbi.gov>
2023-07-10 13:15:58 -0400

    Test Code

---
+++ sudoku_solver.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import curses
+import time
+
+def init_board(board={}):
+	print("Hello World")
+	for x in range(1,10):
+		board[x]={}
+		for y in range(1,10):
+			board[x][y]=-1
+
+def main(stdscr):
+	# Initialization
+	curses.curs_set(0)	# Hide the cursor
+	stdscr.nodelay(True)  # Set non-blocking input
+
+	# Calculate delay in seconds for 10 refreshes per second
+	delay = 1 / 10
+	board={}
+	init_board(board=board)
+
+	# Main loop
+	while True:
+		# Clear the screen
+		stdscr.clear()
+
+		# Get user input
+		key = stdscr.getch()
+
+		# Quit the program if 'q' is pressed
+		if key == ord('q'):
+			break
+
+		# Display a message
+		stdscr.addstr(0, 0, "Welcome to Sudoku Solver (Press 's' to solve, 0 to clear, 'q' to quit, and numbers to fill in the current position)\n\n")
+
+		for y in range(1,10):
+			for x in range(1,10):
+				if not board[x][y]==-1:
+					stdscr.addstr(x*3, y*3, str(board[x][y]))
+				else:				
+					stdscr.addstr(x*3, y*3, "0")
+
+		# Refresh the screen
+		stdscr.refresh()
+
+		# Sleep for the specified delay
+		time.sleep(delay)
+
+if __name__ == '__main__':
+	# Initialize curses and run the main function
+	curses.wrapper(main)
+
