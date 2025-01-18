bd3c7ef4b71082ba7254d172351fb5e9be0a56c7
Jason Hall <jasonbrianhall@gmail.com>
2025-01-18 11:25:09 -0500

    Latest

--- win_main.cpp
+++ win_main.cpp
@@ -51,6 +51,14 @@ ref class SudokuWrapper {
     nativeSudoku->SaveToFile(std::string(wstr.begin(), wstr.end()));
   }
 
+  // Sudoku Puzzle Generation Code   
+  void GenerateEasy_Click(Object ^ sender, EventArgs ^ e);
+  void GenerateMedium_Click(Object ^ sender, EventArgs ^ e);
+  void GenerateHard_Click(Object ^ sender, EventArgs ^ e);
+  void GenerateExpert_Click(Object ^ sender, EventArgs ^ e);
+  void GenerateMaster_Click(Object ^ sender, EventArgs ^ e);
+
+
   // Basic solving techniques
   void StdElim() { nativeSudoku->StdElim(); }
   void LinElim() { nativeSudoku->LinElim(); }
@@ -69,12 +77,6 @@ ref class SudokuWrapper {
   }  // This seems broken right now
 };
 
-protected:
-void GenerateEasy_Click(Object ^ sender, EventArgs ^ e);
-void GenerateMedium_Click(Object ^ sender, EventArgs ^ e);
-void GenerateHard_Click(Object ^ sender, EventArgs ^ e);
-void GenerateExpert_Click(Object ^ sender, EventArgs ^ e);
-void GenerateMaster_Click(Object ^ sender, EventArgs ^ e);
 
 public
 ref class MainForm : public System::Windows::Forms::Form {
