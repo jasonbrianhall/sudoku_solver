32e4028909457316c7d2f566fe5046e4a86e3198
Jason Hall <jasonbrianhall@gmail.com>
2025-01-24 14:13:35 -0500

    Static removed

--- sudoku.cpp
+++ sudoku.cpp
@@ -251,7 +251,7 @@ void Sudoku::RestoreBoard(int original_board[9][9][9], int board[9][9][9]) {
 } 
 
 #ifdef _WIN32
-static DebugQueue debugQueue;
+DebugQueue debugQueue;
 
 
 void Sudoku::print_debug(const char* format, ...) {
