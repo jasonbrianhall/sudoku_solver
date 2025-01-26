e294294bfde1b46b0c41d31d7de8ef9b5c705802
Jason Hall <jasonbrianhall@gmail.com>
2025-01-26 18:59:04 -0500

    Some Basic Solving

--- python_generate_puzzles/sudoku_generator.py
+++ python_generate_puzzles/sudoku_generator.py
@@ -9,14 +9,23 @@ class SudokuPuzzleGenerator:
     def generate_puzzle(self, difficulty="easy"):
         """Generate a new Sudoku puzzle with given difficulty
         Available difficulties: easy, medium, hard, expert, extreme, ultraextreme"""
+        # Generate the puzzle
         if not self.generator.generate_puzzle(difficulty):
             raise Exception("Failed to generate puzzle with difficulty: " + difficulty)
-        return self._get_current_grid(), self._get_solution_grid()
+            
+        # Get unsolved puzzle state
+        puzzle = self._get_current_grid()
+        
+        # Solve to get solution
+        if self.solver.solve() != 0:
+            raise Exception("Failed to solve puzzle")
+            
+        # Get solution grid
+        solution = self._get_current_grid()
+        
+        return puzzle, solution
     
     def _get_current_grid(self):
-        return [[self.solver.get_value(i, j) for j in range(9)] for i in range(9)]
-
-    def _get_solution_grid(self):
         return [[self.solver.get_value(i, j) for j in range(9)] for i in range(9)]
 
     def create_word_document(self, num_puzzles=1, filename="sudoku_puzzles.docx", difficulty="easy"):
@@ -37,7 +46,7 @@ class SudokuPuzzleGenerator:
                 for j in range(9):
                     cell = table.cell(i, j)
                     value = puzzle[i][j]
-                    cell.text = str(value + 1) if value > 0 else ''
+                    cell.text = str(value + 1) if value >= 0 else ''
                     
             doc.add_paragraph()
             
@@ -48,7 +57,8 @@ class SudokuPuzzleGenerator:
             for i in range(9):
                 for j in range(9):
                     cell = table.cell(i, j)
-                    cell.text = str(solution[i][j] + 1)
+                    value = solution[i][j]
+                    cell.text = str(value + 1) if value >= 0 else ''
                     
             doc.add_page_break()
             
