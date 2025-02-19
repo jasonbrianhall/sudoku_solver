3aba81068e91f477a1f4b2f2a35e0753376d2f64
Jason Hall <jasonbrianhall@gmail.com>
2025-02-19 13:33:53 -0500

    Latest

--- compile.sh
+++ compile.sh
@@ -12,12 +12,12 @@ popd
 
 echo "Compiling Various Python Versions"
 pushd python_generate_puzzles
-sh compile.sh
+make
 popd
 
 echo "Compiling Windows CLI Versions"
 pushd windows_cli
-sh compile.sh
+make
 popd
 
 echo -en "\n\nTo compile the windows form version, open SudokuSolver.vcxproj in Visual Studio
