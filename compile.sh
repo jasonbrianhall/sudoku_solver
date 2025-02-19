1a554c9d80f78f9bfce279a3e00dca1bf40ff717
Jason Hall <jasonbrianhall@gmail.com>
2025-02-19 12:34:56 -0500

    Latest

--- compile.sh
+++ compile.sh
@@ -22,7 +22,7 @@ if command -v x86_64-w64-mingw32-g++ &> /dev/null; then
     mkdir win_sudoku_pdcurses -p
 
     echo "Compiling Windows version (PDCurses) ..."
-    x86_64-w64-mingw32-g++ main.cpp sudoku.cpp generatepuzzle.cpp pdcursesprint.cpp curses.cpp -lpdcurses -std=c++14 -o win_sudoku_pdcurses/sudoku_solver_pdcurses.exe
+    x86_64-w64-mingw32-g++ main.cpp sudoku.cpp generatepuzzle.cpp pdcursesprint.cpp -lpdcurses -std=c++14 -o win_sudoku_pdcurses/sudoku_solver_pdcurses.exe
     ./collect_dlls.sh win_sudoku_pdcurses/sudoku_solver_pdcurses.exe  /usr/x86_64-w64-mingw32/sys-root/mingw/bin win_sudoku_pdcurses
 
 else
