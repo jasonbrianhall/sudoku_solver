40b495044b454220c6eca1b9e19f269e4c19ef27
Jason Hall <jasonbrianhall@gmail.com>
2026-05-25 17:19:30 -0400

    Latest

--- windows/sudoku_game.cpp
+++ windows/sudoku_game.cpp
@@ -63,6 +63,7 @@ private:
     PuzzleGenerator* puzzleGenerator;
     array<array<SudokuButton^>^>^ buttons;
     Label^ statusLabel;
+    Panel^ boardPanel;
     String^ currentDifficulty;
     array<array<bool>^>^ immutableCells;
     
